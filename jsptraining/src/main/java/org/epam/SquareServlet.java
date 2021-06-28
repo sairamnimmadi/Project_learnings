52b33487d36546552eca2e3ee3cdfa24d8a22691
sairamnimmadi <sairamnimmadi@gmail.com>
2021-06-28 12:01:40 +0530

    small changes

--- jsptraining/src/main/java/org/epam/SquareServlet.java
+++ jsptraining/src/main/java/org/epam/SquareServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 @WebServlet("/square")
 public class SquareServlet extends HttpServlet {
 
